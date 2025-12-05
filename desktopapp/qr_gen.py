b3bd74342601e19e5148e23c5ea9c22a2c6e23ca
aditii-s <aditi22004@gmail.com>
2025-12-05 15:02:20 +0530

    created website

--- desktopapp/qr_gen.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-
-def generate_qr(data, filename="qr_code.png"):
-    qr = qrcode.QRCode(version=1, box_size=10, border=4)
-    qr.add_data(data)
-    qr.make(fit=True)
-    img = qr.make_image(fill="black", back_color="white")
-    img.save(filename)
-    return filename
--- desktopapp/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-import base64
-
-def generate_aes_key():
-    key = os.urandom(32)  # AES-256
-    return base64.b64encode(key).decode()
-
-def decode_aes_key(key_str):
-    return base64.b64decode(key_str)
